36a82e183d306b574f04a7e9de22616828cfb15d
Xander Salathe <xsalathe@gmail.com>
2024-12-04 12:51:23 +1100

    feat: more robust check for incident presence

--- src/utils/checkIncidentPresence.js
+++ src/utils/checkIncidentPresence.js
@@ -1,9 +1,29 @@
-async function checkIncidentPresence(model, query) {
-    const incident = await model.findOne(query);
-    if (!incident) {
-        throw new Error('Incident not found.');
+const { logError } = require('../functions/logError');
+
+/**
+ * Checks if a document exists in the database based on a provided query.
+ * Throws an error if the document is not found.
+ *
+ * @param {Function} serviceFunction - The service function to fetch the document.
+ * @param {Object} query - The query to find the document.
+ * @param {String} errorMessage - Custom error message to throw if not found.
+ * @returns {Object} - The document found in the database.
+ */
+async function checkIncidentPresence(
+    serviceFunction,
+    query,
+    errorMessage = 'Incident not found.'
+) {
+    try {
+        const incident = await serviceFunction(query);
+        if (!incident) {
+            throw new Error(errorMessage);
+        }
+        return incident;
+    } catch (error) {
+        logError('Checking incident presence', error);
+        throw new Error(errorMessage);
     }
-    return incident;
 }
 
 module.exports = checkIncidentPresence;
