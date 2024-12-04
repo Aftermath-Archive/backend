ee09ecc0cbf8da119affd15fa13681723c256243
Xander Salathe <xsalathe@gmail.com>
2024-12-04 13:01:37 +1100

    fix: updated function names

--- src/routes/incidentRoutes.js
+++ src/routes/incidentRoutes.js
@@ -6,41 +6,53 @@ const incidentController = require('../controllers/incidentController');
 const validateIncidentMiddleware = require('../middlewares/validateIncidentMiddleware');
 const validateDiscussionMiddleware = require('../middlewares/validateDiscussionMiddleware');
 const paginationMiddleware = require('../middlewares/paginationMiddleware');
-const validateObjectId = require('../middlewares/validateObjectId');
+const validateObjectIdMiddleware = require('../middlewares/validateObjectIdMiddleware');
 
 // Create a new incident
-router.post('/', validateIncidentMiddleware, incidentController.createIncident);
+router.post(
+    '/',
+    validateIncidentMiddleware,
+    incidentController.handleCreateIncident
+);
 
 // Get Incident by ID
-router.get('/:id', validateObjectId, incidentController.getIncidentById);
+router.get(
+    '/:id',
+    validateObjectIdMiddleware,
+    incidentController.handleGetIncidentById
+);
 
 // Get all incidents with pagination
-router.get('/', paginationMiddleware, incidentController.getAllIncidents);
+router.get('/', paginationMiddleware, incidentController.handleGetAllIncidents);
 
 // Search and filter incidents
 router.get(
     '/search',
     paginationMiddleware,
-    incidentController.getFilteredIncidents
+    incidentController.handleSearchIncidents
 );
 
 // Update incident by ID
 router.patch(
     '/:id',
-    validateObjectId,
+    validateObjectIdMiddleware,
     validateIncidentMiddleware,
-    incidentController.updateIncident
+    incidentController.handleUpdateIncident
 );
 
 // Delete incident by ID
-router.delete('/:id', validateObjectId, incidentController.deleteIncident);
+router.delete(
+    '/:id',
+    validateObjectIdMiddleware,
+    incidentController.handleDeleteIncident
+);
 
 // Add case discussion to incident
 router.post(
     '/:id/discussion',
-    validateObjectId,
+    validateObjectIdMiddleware,
     validateDiscussionMiddleware,
-    incidentController.addDiscussion
+    incidentController.handleAddDiscussion
 );
 
 module.exports = router;
