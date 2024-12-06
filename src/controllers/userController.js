eb66749c237ef8199e1949f0ace3815fe7d07ddf
Xander Salathe <xsalathe@gmail.com>
2024-12-06 15:04:27 +1100

    feat: initial file setup

---
+++ src/controllers/userController.js
@@ -0,0 +1,13 @@
+// const logError = require('../utils/logError');
+
+// TODO
+
+// handleCreateUser
+
+// handleGetAllUsers
+
+// handleGetUserById
+
+// handleUpdateUser
+
+// handleDeleteUser
---
+++ src/services/userService.js
@@ -0,0 +1,11 @@
+// TODO
+
+// createNewUserService
+
+// findUserByQueryService
+
+// findUsersByQueryService
+
+// updateUserByQueryService
+
+// deleteUserByQueryService
