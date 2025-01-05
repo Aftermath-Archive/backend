00080596a161b6670226d796bd5886c1a376f06e
Xander Salathe <xsalathe@gmail.com>
2025-01-05 20:33:12 +1100

    fix: cors

--- src/server.js
+++ src/server.js
@@ -1,22 +1,36 @@
 const express = require('express');
 const app = express();
-
 const cors = require('cors');
+
+// CORS options
 let corsOptions = {
     origin: [
         'http://localhost:3000', // CRA local
         'http://localhost:5173', // vite local
-        'http://aftermath-archive.xyz/', // production
-        'https://aftermath-archive.xyz/', // production - https
-
+        'http://aftermath-archive.xyz', // production
+        'https://aftermath-archive.xyz', // production - https
     ],
     optionsSuccessStatus: 200,
 };
 
-// middleware setup
+// Middleware setup
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors(corsOptions)); // CORS middleware
 app.use(express.urlencoded({ extended: true }));
+
+// Handle CORS preflight requests
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', req.headers.origin); // Dynamically allow the requesting origin
+    res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, OPTIONS'
+    );
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200); // Respond with HTTP 200 for preflight requests
+    }
+    next();
+});
 
 /**
  * swagger configuration
